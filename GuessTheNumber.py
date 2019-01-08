4b5e48d6b667e7b7e5ed21c8cf9bc3d82928f568
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-08 21:04:50 +0100

    small changes and upgrades

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,38 +1,45 @@
 #This will be a "Guess the number" game.
 
 import random
+import time
 
 guessesTaken = 0
 
-print ('Hi there! In a sec you will play with me in a "Guess the number" game. I will choose some number beetwen 1 and 20 and you will have 6 chances to guess my number.\nPlease tell me your name.')
+print ('Hi there! In a sec you will play with me in a "Guess the number" game. \
+I will choose some number beetwen 1 and 20 and you will have \
+6 chances to guess my number.\nPlease tell me your name.')
 name = input()
 print ('\nNice to meet you', name + '!')
 
-number = random.randint(1,20)
-print ('\nSo, please tell me what number I am thinking about right now beetwen 1 and 20?')
+randomNumber = random.randint(1,20)
+print ('\nSo, please tell me what number I am thinking about right now beetwen \
+1 and 20?')
 
-for guessesTaken in range(6):
+for guessesTaken in range(5):
     guess = input('Take a guess: ')
     guess = int(guess)
 
-    if guess < number:
+    if guess < randomNumber:
         print ('Your guess is too low.')
 
-    if guess > number:
+    if guess > randomNumber:
         print ('Your guess is too high.')
 
-    if guess == number:
+    if guess == randomNumber:
         break
 
-if guess == number:
+if guess == randomNumber:
     guessesTaken = str(guessesTaken + 1)
-    print ('\nAmazing ' +name+ '! ' 'You guessed my number in ' +guessesTaken+ ' guesses.')
+    print ('\nAmazing ' +name+ '! ' 'You guessed my number in ' \
+           +guessesTaken+ ' guesses.')
     print ('\nPlay again!')
 
-if guess != number:
-    number = str(number)
-    print ('\nSorry, but my number was ' +number+ '.')
+if guess != randomNumber:
+    randomNumber = str(randomNumber)
+    print ('\nSorry, but my number was ' +randomNumber+ '.')
     print ('\nDon\'t worry. Try again!')
 
 input('\nThanks for playing! Press ENTER to exit and play again!.')
-ngn
+time.sleep(2)
+exit()
+
