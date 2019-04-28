4129187858abf1fdad0cae0e52ed7c428819aa9a
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-28 13:46:46 +0100

    continue

--- Desktop/PythonProjects/Checkit/check.py
+++ Desktop/PythonProjects/Checkit/check.py
@@ -2,6 +2,15 @@
 
 """check.py - Launches google.com with words from the command line or using clipboard."""
 
-# TODO: Import essential modules.
-# TODO: Set searched_words variable using command line or clipboard.
+# Import essential modules.
+import sys
+import pyperclip
+import webbrowser
+
+# Set searched_words variable using command line or clipboard.
+if len(sys.argv) > 1:
+    searched_words = ' '.join(sys.argv[1:])
+else:
+    searched_words = pyperclip.paste()
+
 # TODO: Open browser with provided words.
