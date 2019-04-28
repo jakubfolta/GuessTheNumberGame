1cac1138e1f03c006ddcfdc411d2aff2889ce511
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-28 13:42:21 +0100

    continue

---
+++ Desktop/PythonProjects/Checkit/check.py
@@ -0,0 +1,7 @@
+#! python3
+
+"""check.py - Launches google.com with words from the command line or using clipboard."""
+
+# TODO: Import essential modules.
+# TODO: Set searched_words variable using command line or clipboard.
+# TODO: Open browser with provided words.
